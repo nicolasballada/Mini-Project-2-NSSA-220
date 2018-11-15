821e429c21b42ba252f559d15c0799efa200e52a
Nicolas Salomon <31396062+nsalomonb@users.noreply.github.com>
2018-11-15 16:25:20 -0500

    Start of filter

--- filter_packets.py
+++ filter_packets.py
@@ -1,3 +1,17 @@
-def filter() :
-	print 'called filter function in filter_packets.py'
+import sys
+
+def filter(fn) :
+    fileName = sys.argv[1]
+    read( fileName )
+	
+def read(fn) :
+    file = open(fn, 'r')
+    for line in file :
+        if 'ICMP' in line :
+        print("got it")
+    file.close()
+	
+	
+	
+	
 	
