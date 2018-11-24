77f6cf047361ef6990213636b18ffa3316980408
Nicolas Salomon <31396062+nsalomonb@users.noreply.github.com>
2018-11-24 11:47:54 -0500

    Update filter_packets.py

--- filter_packets.py
+++ filter_packets.py
@@ -1,16 +1,31 @@
+#!/bin/python
+
 import sys
 
-def filter( fileName ) :
-    read( fileName )
-	
-def read(fn) :
-    file = open(fn, 'r')
-    for line in file :
-        if 'ICMP' in line :
-        print("got it")
-    file.close()
-	
-	
+def filter(fn):
+    read(fn)
+
+def read(file_name):
+    mid_step = file_name.split(".")
+    new_file_name = mid_step[0] + "_filtered." + mid_step[1]
+
+    unfiltered_node = open(file_name, "r")
+    filtered_node = open(new_file_name, "a")
+
+    line = unfiltered_node.readline()
+
+    while line:
+        if "ICMP" in line:
+            filtered_node.write(line)
+            line = unfiltered_node.readline()
+            filtered_node.write(line)
+        line = unfiltered_node.readline()
+
+
+fn = "file.txt"
+
+init(fn)
+
 	
 	
 	
