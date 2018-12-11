b43a9d1da1af43632778c0152427f81893a5bca2
mxt2842 <35978876+mxt2842@users.noreply.github.com>
2018-12-11 17:41:27 -0500

    Update compute_metrics.py
    added distance metrics

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,6 +1,23 @@
+node1IP = "192.168.100.1"
+node2IP = "192.168.100.2"
+node3IP = "192.168.200.1"
+node4IP = "192.168.200.2"
+
+node1 = []
+node2 = []
+node3 = []
+node4 = []
+node5 = []
+
 def compute(L) :
 	print 'called compute function in compute_metrics.py'
 	computing(L)
+
+def parse(file_name, L):
+    file = open(file_name, "r")
+    for line in file:
+        L.append(line.split())
+    file.close()
 	
 def computing(L):
 	reqsent = 0;
@@ -30,9 +47,76 @@ def computing(L):
 	
 	print "Replies Sent: " + str(repsent)
 	print "Replies Recieved: " + str(reprecieved)
-	
-	
-			
+
+def numRequestBytesSent(ip):
+    total = 0
+    for n1 in node1:
+        if n1[2] == ip and n1[8] == "request":
+            total = total + int(n1[5])
+    for n2 in node2:
+        if n2[2] == ip and n1[8] == "request":
+            total = total + int(n2[5])
+    for n3 in node3:
+        if n3[2] == ip and n1[8] == "request":
+            total = total + int(n3[5])
+    for n4 in node4:
+        if n4[2] == ip and n1[8] == "request":
+            total = total + int(n4[5])
+    return total
+
+def numRequestBytesRecieved(ip):
+    total = 0
+    for n1 in node1:
+        if n1[3] == ip and n1[8] == "request":
+            total = total + int(n1[5])
+    for n2 in node2:
+        if n2[3] == ip and n1[8] == "request":
+            total = total + int(n2[5])
+    for n3 in node3:
+        if n3[3] == ip and n1[8] == "request":
+            total = total + int(n3[5])
+    for n4 in node4:
+        if n4[3] == ip and n1[8] == "request":
+            total = total + int(n4[5])
+    return total
+
+def numRequestDataSent(ip):
+    total = 0
+    for n1 in node1:
+        if n1[2] == ip and n1[8] == "request":
+            total = total + int(n1[5]) - 42
+    for n2 in node2:
+        if n2[2] == ip and n1[8] == "request":
+            total = total + int(n2[5]) - 42
+    for n3 in node3:
+        if n3[2] == ip and n1[8] == "request":
+            total = total + int(n3[5]) - 42
+    for n4 in node4:
+        if n4[2] == ip and n1[8] == "request":
+            total = total + int(n4[5]) - 42
+    return total
+
+def numRequestDataRecieved(ip):
+    total = 0
+    for n1 in node1:
+        if n1[3] == ip and n1[8] == "request":
+            total = total + int(n1[5]) - 42
+    for n2 in node2:
+        if n2[3] == ip and n1[8] == "request":
+            total = total + int(n2[5]) - 42
+    for n3 in node3:
+        if n3[3] == ip and n1[8] == "request":
+            total = total + int(n3[5]) - 42
+    for n4 in node4:
+        if n4[3] == ip and n1[8] == "request":
+            total = total + int(n4[5]) - 42
+    return total
+
+parse("Node1_filtered.txt", node1)
+parse("Node2_filtered.txt", node2)
+parse("Node3_filtered.txt", node3)
+parse("Node4_filtered.txt", node4)
+parse("Node5_filtered.txt", node5)
 
 		
 # Node 1 - 192.168.100.1
