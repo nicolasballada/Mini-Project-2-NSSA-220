8938c3cbbc1becfa3ed5fc9f1d9b1c841c7b2245
nsalomonb <nsalomon.b@gmail.com>
2018-12-11 23:16:56 -0500

    packet analyzer update

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -7,10 +7,20 @@ import sys
 fileName = sys.argv[1]
 
 L = []
+ip = "192.168.100.1"
+
+if( "1" in fileName):
+    ip = "192.168.100.1"
+elif( "2" in fileName):
+    ip = "192.168.100.2"
+elif( "3" in fileName):
+    ip = "192.168.200.1"
+elif( "4" in fileName):
+    ip = "192.168.200.2"
+elif( "5" in fileName):
+    ip = "192.168.100.254"
 
 filter(fileName)
 parse(fileName, L)
-compute(L)
-
-
+compute(L, ip, fileName)
 
